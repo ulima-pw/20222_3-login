bd821e33b6ad79875730b7b3bb988de9cea10fd8
ProfN406 <ProfN406@lab.aca>
2022-09-26 12:04:12 -0500

    Agregando funcionalidad de boton

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,19 +1,36 @@
+import React, { useState } from "react"
+
 const LoginPage = () => {
+    // Variables de estado
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
     return <div className="container">
         <h1>Login</h1>
         <div>
             <label className="form-label">
                 Usuario
             </label>
-            <input className="form-control" />
+            <input className="form-control"
+                value={ username }
+                onChange={ (evt) => setUsername(evt.target.value) } />
         </div>
         <div>
             <label className="form-label">
                 Password
             </label>
-            <input className="form-control" />
+            <input type="password" 
+                className="form-control"
+                value={ password }
+                onChange={ (evt) => setPassword(evt.target.value) } />
         </div>
-        <button className="btn btn-primary" type="button">Login</button>
+        <button className="btn btn-primary" 
+            type="button"
+            onClick={ 
+                () => console.log(`Username: ${username} Password: ${password}`) 
+            } >
+                Login
+        </button>
     </div>
 }
 
