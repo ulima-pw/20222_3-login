e1784ed7fbd7462f71afec9698b3bed8a3091a7a
ProfN406 <ProfN406@lab.aca>
2022-09-26 11:30:47 -0500

    Import inicial

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,14 @@
+const LoginPage = () => {
+    return <div>
+        <h1>Login</h1>
+        <div>
+            <input />
+        </div>
+        <div>
+            <input />
+        </div>
+        <button type="button">Login</button>
+    </div>
+}
+
+export default LoginPage
